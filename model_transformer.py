d4e37ea7b90b87747f2454d56315edefbcb29983
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2022-06-08 14:11:06 -0400

    Add attention masking and layer norm

--- model_transformer.py
+++ model_transformer.py
@@ -5,8 +5,10 @@ import numpy as np
 import torch 
 from torch import nn, Tensor
 import torch.nn.functional as F
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.nn import TransformerEncoder, TransformerEncoderLayer, LayerNorm
 from torchvision import models
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available else "cpu")
 
 class PositionalEncoding(nn.Module):
 
@@ -31,16 +33,32 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:x.size(0)]
         return self.dropout(x)
 
+class ResidueGate(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    def forward(self, x, y):
+        return x + y    
+
+def generate_square_subsequent_mask(sz: int) -> Tensor:
+    """Generates an upper-triangular matrix of -inf, with zeros on diag."""
+    return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1).to(DEVICE)
+
 class BERT_Torch(nn.Module):
 
     def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
-                 nlayers: int, dropout: float = 0.5):
+                 nlayers: int, dropout: float = 0.5,
+                 layer_norm_eps: float = 1e-5):
         super().__init__()
+
         self.pos_encoder = PositionalEncoding(d_model, dropout)
         encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        encoder_norm = LayerNorm(d_model, eps=layer_norm_eps)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers, encoder_norm)
+
         self.d_model = d_model
-        self.decoder = nn.Linear(d_model, ntoken)
+
+        # self.decoder = nn.Linear(d_model, ntoken)
 
     def forward(self, src: Tensor) -> Tensor:
         """
@@ -51,6 +69,7 @@ class BERT_Torch(nn.Module):
             output Tensor of shape [seq_len, batch_size, ntoken]
         """
         src = self.pos_encoder(src)
-        output = self.transformer_encoder(src)
-        output = self.decoder(output)
+        print(src.shape)
+        output = self.transformer_encoder(src, generate_square_subsequent_mask(src.size(0)))
+        # output = self.decoder(output)
         return output
