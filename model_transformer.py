757257e9c804cee838bbef23f1945ec226cefbfe
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2022-07-19 10:32:30 -0400

    Rewrite PE + readd BERT

--- model_transformer.py
+++ model_transformer.py
@@ -11,32 +11,52 @@ from typing import Optional
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available else "cpu")
 
 
+# class PositionalEncoding(nn.Module):
+#     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+#         super().__init__()
+#         self.dropout = nn.Dropout(p=dropout)
+
+#         position = torch.arange(max_len).unsqueeze(1)
+
+#         # This PE implementation is 5x faster than following Attention is All You Need formula.
+#         div_term = torch.exp(
+#             torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
+#         )
+#         # add positional embedding directly to the state/state+action embedding
+#         # note: dim is a bit different from NLP embeddings
+#         pe = torch.zeros(max_len, d_model)
+#         pe[:, 0::2, :] = torch.sin(position * div_term)
+#         pe[:, 1::2 ] = torch.cos(position * div_term)
+#         self.register_buffer("pe", pe)
+
+#     def forward(self, x: Tensor) -> Tensor:
+#         """
+#         Args:
+#             x: Tensor, shape [seq_len, batch_size, embedding_dim]
+#         """
+#         x = x + self.pe[: x.size(0)]
+#         return self.dropout(x)
+
 class PositionalEncoding(nn.Module):
+
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
 
         position = torch.arange(max_len).unsqueeze(1)
-
-        # This PE implementation is 5x faster than following Attention is All You Need formula.
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
-        )
-        # add positional embedding directly to the state/state+action embedding
-        # note: dim is a bit different from NLP embeddings
-        pe = torch.zeros(max_len, d_model)
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        self.register_buffer("pe", pe)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
 
     def forward(self, x: Tensor) -> Tensor:
         """
         Args:
             x: Tensor, shape [seq_len, batch_size, embedding_dim]
         """
-        x = x + self.pe[: x.size(0)]
+        x = x + self.pe[:x.size(0)]
         return self.dropout(x)
-
 
 class ResidueGate(nn.Module):
     def __init__(self, **kwargs):
@@ -48,8 +68,49 @@ class ResidueGate(nn.Module):
 
 def generate_square_subsequent_mask(sz: int) -> Tensor:
     """Generates an upper-triangular matrix of -inf, with zeros on diag."""
-    return torch.triu(torch.ones(sz, sz) * float("-inf"), diagonal=1).to(DEVICE)
+    return torch.triu(torch.ones(sz, sz) * float("-inf"), diagonal=1)
 
+class BERT(nn.Module):
+    def __init__(
+        self,
+        ntoken: int,
+        d_model: int,
+        nhead: int,
+        d_hid: int,
+        nlayers: int,
+        dropout: float = 0.5,
+        layer_norm_eps: float = 1e-5,
+    ):
+        super().__init__()
+
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(
+            d_model, nhead, d_hid, dropout, batch_first=True
+        )
+        encoder_norm = LayerNorm(d_model, eps=layer_norm_eps)
+        self.transformer_encoder = TransformerEncoder(
+            encoder_layers, nlayers, encoder_norm
+        )
+
+        self.d_model = d_model
+
+        # self.decoder = nn.Linear(d_model, ntoken)
+
+    def forward(self, src: Tensor) -> Tensor:
+        """
+        Args:
+            src: Tensor, shape [seq_len, batch_size]
+        Returns:
+            output Tensor of shape [seq_len, batch_size, ntoken]
+        """
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(
+            src, generate_square_subsequent_mask(src.size(-2))
+        )
+        # print("out",output)
+        # print(output.shape)
+        # output = self.decoder(output)
+        return output
 
 class GTrXLLayer(nn.TransformerEncoderLayer):
     def __init__(
@@ -61,7 +122,7 @@ class GTrXLLayer(nn.TransformerEncoderLayer):
         activation="relu",
         dropout=0,
         layer_norm_eps=1e-5,
-        batch_first=False,
+        batch_first=True,
     ):
         super().__init__(
             d_model,
